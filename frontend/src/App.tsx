a2499c5db13a2c5041562f21d09f7edb81d47036
UmmaraAmin-byte <aminummara@gmail.com>
2025-11-26 18:08:06 +0500

    Initial commit frontend + backend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center">
+      <h1 className="text-3xl font-bold">Tailwind + Vite Working!</h1>
+    </div>
+  );
+}
+
+export default App;
